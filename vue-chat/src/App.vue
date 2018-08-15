e626639df2c05a308e86efabbcc988419b1a481b
Pham Tuan <phamtanchip@gmail.com>
2018-08-16 01:31:36 +0700

    add example

---
+++ vue-chat/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+   <div>
+    <Initialize />
+  </div> 
+</template>
+
+<script>
+import Initialize from "./Initialize.vue";
+export default {
+  components: {
+    Initialize
+  },
+  name: "app",
+  created() {
+    console.log(this.$store.state.db);
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ vue-chat/src/config.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyC27QgTpSc2JsjwCV9VQWVD7hLaNj9eU30",
+    authDomain: "vuejs-chat-firebase.firebaseapp.com",
+    databaseURL: "https://vuejs-chat-firebase.firebaseio.com",
+    projectId: "vuejs-chat-firebase",
+    storageBucket: "vuejs-chat-firebase.appspot.com",
+    messagingSenderId: "654191283502"
+  };
+  export default config
