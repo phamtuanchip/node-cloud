cf1e29ac2799cd498f8d158511e528640019cba4
pham tuan <phamtuanchip@gmail.com>
2018-10-12 11:07:32 +0700

    add more book and example

--- node-oauth2-mongo-server/app.js
+++ node-oauth2-mongo-server/app.js
@@ -9,7 +9,7 @@ var oauthserver = require('node-oauth2-server');
 var User = models.User;
 
 app.set('env', process.env.NODE_ENV || 'development');
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 3004);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.use(express.cookieParser('ncie0fnft6wjfmgtjz8i'));
